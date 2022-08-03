0293927085923aed05054310311b38a9e2192269
Nathan <nbn22385@gmail.com>
2022-08-03 13:56:22 -0400

    install script checks for ansible-playbook installation before proceeding

--- install.sh
+++ install.sh
@@ -1,3 +1,26 @@
 #!/usr/bin/env bash
 
+ansible_executable=ansible-playbook
+
+if ! [ -x "$(command -v $ansible_executable)" ]; then
+  read -p "[+] $ansible_executable was not found, install? [y/n]: " install_ansible
+  if [[ "$install_ansible" == [yY] || "$install_ansible" == [yY][eE][sS] ]]; then
+    if [[ $OSTYPE == 'darwin'* ]]; then
+      echo "[+] Installing ansible with homebrew"
+      brew install ansible
+    elif [ -x "$(command -v apt)" ]; then
+      apt install -y ansible
+    else
+      echo "[-] Not installing Ansible. No supported package manager was found."
+      exit 1
+    fi
+  else
+    echo "[-] ansible-playbook will not be installed, exiting."
+    exit 2;
+  fi
+else
+  echo "[+] ansible-playbook found, proceeding with install."
+fi
+
 ansible-galaxy install -r requirements.yml
+ansible-playbook playbook.yml
